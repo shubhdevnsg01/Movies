9778a7aae3a489adbb23d3cf32c12afc4f8be8e9
shubhdevnsg01 <petrifiedmaniac@gmail.com>
2023-12-12 10:45:17 +0530

    first commit

---
+++ movie-app-backend/cmd/api/handlers.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"backend/cmd/api/internal/models"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
+	var payload = struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Movies up and running",
+		Version: "1.0.0",
+	}
+
+	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "2009-03-07")
+
+	ChakDeIndia := models.Movie{
+		ID:          1,
+		Title:       "ChakDeIndia",
+		ReleaseDate: rd,
+		MPAARating:  "R",
+		RunTime:     116,
+		Description: "A very nice movie",
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	}
+
+	movies = append(movies, ChakDeIndia)
+
+	out, err := json.Marshal(movies)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
